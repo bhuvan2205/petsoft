ee52463b2abc4496616788855a3ee898a1b5a165
bhuvan2205 <bhuvancs2205@gmail.com>
2024-04-12 13:39:50 +0530

    Fix: Redirection rules for the payment

--- src/lib/auth-edge.ts
+++ src/lib/auth-edge.ts
@@ -9,45 +9,43 @@ export const nextAuthEdgeConfig = {
 		// Runs every request
 		authorized: ({ auth, request }) => {
 			const isLoggedIn = Boolean(auth?.user);
-			const isPrivateRoute = request.nextUrl.pathname.includes("/app");
-			const isPaymentRoute = request.nextUrl.pathname.includes("/payment");
+			const isAppRoute = request.nextUrl.pathname.includes("/app");
 			const isLoginRoute = request.nextUrl.pathname.includes("/login");
 			const isSignUpRoute = request.nextUrl.pathname.includes("/signup");
 
-			if (isLoggedIn) {
-				if (isPrivateRoute) {
-					if (auth?.user?.hasAccess) {
-						return true;
-					} else {
-						return Response.redirect(new URL("/payment", request.nextUrl));
-					}
-				} else if (isLoginRoute || isSignUpRoute) {
-					if (auth?.user?.hasAccess) {
-						return Response.redirect(
-							new URL("/app/dashboard", request.nextUrl)
-						);
-					} else {
-						return Response.redirect(new URL("/payment", request.nextUrl));
-					}
-				} else if (isPaymentRoute) {
-					if (auth?.user?.hasAccess) {
-						return Response.redirect(
-							new URL("/app/dashboard", request.nextUrl)
-						);
-					} else {
-						return true;
-					}
-				}
-				return true;
-			} else {
-				if (isPaymentRoute) {
-					return Response.redirect(new URL("/login", request.nextUrl));
-				}
-				if (!isPrivateRoute) {
-					return true;
-				}
+			if (!isLoggedIn && isAppRoute) {
 				return false;
 			}
+
+			if (isLoggedIn && isAppRoute && !auth?.user.hasAccess) {
+				return Response.redirect(new URL("/payment", request.nextUrl));
+			}
+
+			if (isLoggedIn && isAppRoute && auth?.user.hasAccess) {
+				return true;
+			}
+
+			if (
+				isLoggedIn &&
+				(isLoginRoute || isSignUpRoute) &&
+				auth?.user.hasAccess
+			) {
+				return Response.redirect(new URL("/app/dashboard", request.nextUrl));
+			}
+
+			if (isLoggedIn && !isAppRoute && !auth?.user.hasAccess) {
+				if (isLoginRoute || isSignUpRoute) {
+					return Response.redirect(new URL("/payment", request.nextUrl));
+				}
+
+				return true;
+			}
+
+			if (!isLoggedIn && !isAppRoute) {
+				return true;
+			}
+
+			return false;
 		},
 		jwt: async ({ token, user, trigger }) => {
 			if (user) {
